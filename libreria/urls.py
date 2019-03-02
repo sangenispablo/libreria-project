ca2d15bda60be8fda4c2a31aafd965a6c110be23
Pablo Sangenis <sangenispablo@gmail.com>
2019-03-02 11:44:03 -0300

    Esqueleto del proyecto

---
+++ libreria/urls.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.urls import path
+
+# Proyecto
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+# App Catalogo
+from django.urls import include
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+#Redirijimos el trafico de la raiz a la app catalogo
+from django.views.generic import RedirectView
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
+
+# Use static() para servir archivos estaticos solo durante el desarrollo
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
